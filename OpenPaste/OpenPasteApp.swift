10c7ad3e1e6d22f1e8c069c91a0f9260b8fa55ff
Lê Anh Tuấn <justin.le.1105@gmail.com>
2026-04-01 15:21:45 +0700

    Initial Commit

---
+++ OpenPaste/OpenPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenPasteApp.swift
+//  OpenPaste
+//
+//  Created by Lê Anh Tuấn on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenPasteTests/OpenPasteTests.swift
@@ -0,0 +1,19 @@
+//
+//  OpenPasteTests.swift
+//  OpenPasteTests
+//
+//  Created by Lê Anh Tuấn on 1/4/26.
+//
+
+import Testing
+@testable import OpenPaste
+
+struct OpenPasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
